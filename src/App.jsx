79d4ae194371c26201545ea6d85e8d48f3229be6
Mahad095 <k200338@nu.edu.pk>
2022-09-06 13:21:45 +0500

    Sider bar completed and functional. font changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Controller, Board } from './Components'
-import { Box } from '@mui/material';
+import { Box, AppBar, Toolbar } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
    typography: {
-    "fontFamily": `'Ubuntu', sans-serif`,
+    'font-family': `'Inter', sans-serif`
    },
    palette: {
     primary : {
@@ -31,15 +31,34 @@ export default function App() {
     }
     const algorithms = {
         "Djikstra" : () => board.current?.setAlgorithm("djikstra"),
-        "A* Search" : () => board.current?.setAlgorithm(null)
+        "A* Search" : () => board.current?.setAlgorithm("djikstra"),
+        "Swarm" : () => board.current?.setAlgorithm("djikstra")
     }
     return (
         <ThemeProvider theme={theme}>
+            {/* <AppBar position = 'static'>
+                <Toolbar>
+                        
+                </Toolbar>
+            </AppBar> */}
             <Box display='flex' width='100vw' height='100vh'>
                 <Controller actions = {actions} algorithms = {algorithms}/>
                 <Board ref={board}/>
             </Box>
         </ThemeProvider>
+        // <ThemeProvider theme={theme}>
+        //     <Box display='flex' width='100vw' height='100vh'>
+        //         <Controller actions = {actions} algorithms = {algorithms}/>
+        //         <Box width = '100%' height = '100%'>
+        //             <AppBar position = 'static'>
+        //                 <Toolbar>
+                                
+        //                 </Toolbar>
+        //             </AppBar>
+        //             <Board ref={board}/>
+        //         </Box>
+        //     </Box>
+        // </ThemeProvider>
         // </Box>
     )
 }
--- src/Components/Controller/Controller.jsx
+++ src/Components/Controller/Controller.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-import { Box, ButtonBase, Drawer } from '@mui/material';
+import { Box, ButtonBase, Drawer, Paper, Avatar, Typography, Button, IconButton } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
 import ClearIcon from '@mui/icons-material/Clear';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 const CustomIconButton = (props) => {
   const {sx, children, ...forwardProps} = props;
@@ -24,49 +27,98 @@ const CustomIconButton = (props) => {
   )
 }
 
-
+const colors = ['primary.main', 'secondary.main', 'tertiary.main'];
+const actionIcons = 
+[ 
+  <PlayArrowIcon fontSize = 'large' color = 'secondary'/>,
+  <PauseIcon fontSize = 'large' color = 'tertiary'/>,
+  <ClearIcon fontSize = 'large' color = 'primary'/>
+]
 export default function Controller({theme, algorithms, actions}) {
   const [open, setopen] = React.useState(false);
+
   console.log(actions);
   return (
-    <Box 
-      display = "flex"
-      flexDirection = "column"
-      width = "100px"
+    <>
+      <Drawer
+        anchor="left"
+        open={open}
+        // variant = 'permanent'
+        onClose={() => setopen(false)}
       >
-      
-      <CustomIconButton 
-        sx={{mx:"auto", mt: 2}}
-        onClick = {()=>setopen(true)}
-        >
-        <MenuOpenIcon fontSize = 'large'  sx={{color:'darkgray'}}/>
-      </CustomIconButton>
+        <Box height = '100%' sx = {{display : 'flex', justifyContent: 'space-between', flexDirection : 'column'}}>
+          <Paper component={ButtonBase}  sx = {{display : "flex", px : 3, py : 2, m:3, mt:4, boxShadow : 3, borderRadius : 4}}>
+            <Avatar sx = {{mr: 3}}>M</Avatar>
+            <Box>
+              <Typography fontWeight='bold'>Mahad Hameed</Typography>
+              <Typography color='textSecondary' textAlign='initial'>Creator</Typography>
+            </Box>
+          </Paper>
+          <Box 
+            >
+            <Typography textAlign = 'center' color='textSecondary' fontWeight='bold'>Choose an algorithm!</Typography>
+            <Box 
+              display = 'flex' 
+              flexDirection = 'column' 
+              width = 'fit-content' 
+              mx='auto'
+            >
+              {
+                Object.keys(algorithms).map( 
+                  (key, index) => <Button 
+                                    variant="contained" 
+                                    sx={{my: 3, px:4, py: 2, borderRadius : 4, color : 'white', fontWeight : 'bold', bgcolor : colors[index]}} 
+                                    key={key}
+                                    >
+                                    {key}
+                                  </Button>)
+              }
+            </Box>
+          </Box>
+          <Box display = 'flex' justifyContent='space-evenly' my={2}>
+            <IconButton>
+              <EmailIcon fontSize='large'/>
+            </IconButton>
+            <IconButton>
+              <GitHubIcon fontSize='large'/>
+            </IconButton>
+            <IconButton>
+              <LinkedInIcon fontSize='large'/>
+            </IconButton>
+          </Box>
+        </Box>
+      </Drawer>
       <Box 
-        display="flex"
-        flexDirection='column'
-        alignItems='center'
-        my='auto'
-      >
+        display = "flex"
+        flexDirection = "column"
+        width = "100px"
+        >
+        
         <CustomIconButton 
-          sx = {{my:4}}
-          onClick = {actions["Start"]}
+          sx={{mx:"auto", mt: 2}}
+          onClick = {()=>setopen(true)}
           >
-          <PlayArrowIcon fontSize = 'large' color = 'secondary'/>
+          <MenuOpenIcon fontSize = 'large'  sx={{color:'darkgray'}}/>
         </CustomIconButton>
-        <CustomIconButton 
-          sx = {{my:4}}
-          onClick = {actions["Stop"]}
-          >
-          <PauseIcon fontSize = 'large' color = 'tertiary'/>
-        </CustomIconButton>
-        <CustomIconButton 
-          sx = {{my:4}}
-          onClick = {actions["Clear"]}
-          >
-          <ClearIcon fontSize = 'large' color = 'primary'/>
-        </CustomIconButton>
+        <Box 
+          display="flex"
+          flexDirection='column'
+          alignItems='center'
+          my='auto'
+        >
+          {
+            Object.keys(actions).map(
+              (key, index) => <CustomIconButton
+                                sx = {{my : 4}} 
+                                onClick = {actions[key]}>
+                                  {
+                                    actionIcons[index]
+                                  }
+                                </CustomIconButton>)
+          }
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }
 
