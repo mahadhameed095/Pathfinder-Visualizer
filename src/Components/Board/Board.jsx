37b47a9d5edeef810095e30ff2958704dd46d118
Mahad095 <k200338@nu.edu.pk>
2022-09-24 16:11:32 +0500

    Some refactoring. Marked private methods with double underscore.

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -2,111 +2,38 @@ import React from "react";
 import Source from '../../Assets/Source.svg';
 import Target from '../../Assets/Target.svg';
 import Animator from "../../Utility/Animator";
+import { getRandomInt } from "../../Utility/Random";
 import { djikstraGen, djikstra} from "../../Algorithms/Djikstra";
 import { astarGen, astar} from "../../Algorithms/Astar";
 import * as CONSTANTS from "../../Utility/constants";
-import recurDiv  from "../../Algorithms/RecursiveDivision";
 import { Grid, IconCell, VisitedCell, NormalCell, Wall, Path } from './Styles'
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
 
 export default class Board extends React.Component{
   constructor(){
     super();
-    this.source = null;
-    this.target = null;
-    this.state = {cells : this.makeBoard(15, 29)};
-    this.grid = React.createRef();
-    this.frames = null;
-    this.done = false;
-    this.isMouseDown = false;
+    this.source = null; /* Coordinates of source node */
+    this.target = null; /* Coordinates of target node */
+    this.state = {cells : this.__makeBoard(15, 29)}; /* Represents the state of the grid(mxn array) */
+    this.grid = React.createRef(); /* The reference to the physical html div that represents the grid. It is used to fetch its dimensions */
+    this.frames = null; /* Holds the current generator object */
+    this.done = false; /* A boolean that is set when the animation is done. and clears while animating */
+    this.isMouseDown = false; 
     this.isDragging = null;
     this.algorithmChoice = "djikstra";
     this.algorithms = {
       "djikstra" : [() => djikstraGen(this.state.cells, this.source, this.target),() => djikstra(this.state.cells, this.source, this.target)],
       "astar" : [ () => astarGen(this.state.cells, this.source, this.target), () => astar(this.state.cells, this.source, this.target)]
     }
-    
-    this.resizeGrid = () => {
-      this.clear();
-      const containerWidth = this.grid.current.offsetWidth;
-      const containerHeight = this.grid.current.offsetHeight;
-      const cols = Math.floor((containerWidth * 29)/1200);
-      const rows = Math.floor(containerHeight / (containerWidth / cols - 1));
-      this.setState({cells:this.makeBoard(rows, cols)});
-      this.forceUpdate();
-    }
-
-    this.animator = new Animator(60, (animatorRef) => {
-      const nextFrame = this.frames.next();
-      let board;
-      if(nextFrame.done){
-        animatorRef.stop();
-        this.frames = null;
-        this.done = true;
-        if(nextFrame.value === null) alert("No pathh!!!");
-        return;
-      } 
-      
-      if(nextFrame.value.type === "visited"){
-        board = this.state.cells.map((row, i) => row.map((item, j) => nextFrame.value.value[[i, j]].visited && item === CONSTANTS.NORMAL ? CONSTANTS.VISITED : item))
-      }
-      else if(nextFrame.value.type === "path"){
-        board = this.updateIndices([...nextFrame.value.value], [CONSTANTS.PATH], this.state.cells);
-      }
-      this.setState({cells:board});
-      this.forceUpdate();
-    });
-  }
-  
-  componentDidMount(){
-    this.resizeGrid();
-    window.addEventListener('resize', this.resizeGrid);
-  }
-  componentWillUnmount(){
-    window.removeEventListener('resize', this.resizeGrid);
-  }
-  canPlay(){
-    return this.isDragging === null;
+    this.animator = new Animator(60, this.__animate);
   }
   isPlaying(){
     return this.animator.playing;
-  }
-  isInteractable(){
-    return this.frames === null;
-  }
-  makeBoard(rows, columns){
-    
-    const board = Array.from({length: rows},()=> Array.from({length: columns}, () => CONSTANTS.NORMAL));
-    this.source = [getRandomInt(0, rows), getRandomInt(0, columns / 2)]
-    do{
-      this.target = [getRandomInt(0, rows), getRandomInt(columns / 2 + 1, columns)]
-    }
-    while(this.target[0] === this.source[0] && this.target[1] === this.source[1]);
-    board[this.source[0]][this.source[1]] = CONSTANTS.SOURCE;
-    board[this.target[0]][this.target[1]] = CONSTANTS.TARGET;
-    return board;
-  }
-  updateIndices(indices, values, array){
-    const board = [...array];
-    if(values.length === 1)
-      indices.map( index => board[index[0]][index[1]] = values[0]);
-    else if(values.length === indices.length)
-      indices.map( (index, i) => board[index[0]][index[1]] = values[i]);
-    else{
-      console.error("Error in updateIndices function. The length of valuelist is incompatible.");
-    }
-    return board;
   }
   setAlgorithm(algorithm){
     this.algorithmChoice = algorithm;
   }
   start(){
-    if(this.frames === null && this.canPlay())
+    if(this.__isInteractable() && this.__canPlay())
     {
       this.done = false;
       this.frames = this.algorithms[this.algorithmChoice][0]();
@@ -121,13 +48,78 @@ export default class Board extends React.Component{
     this.animator.stop();
   }
   clear(){
+    this.done = false;
     this.frames = null;
     this.animator.stop();
     this.setState({
       cells : this.state.cells.map(row => row.map(cell => (cell === CONSTANTS.SOURCE || cell === CONSTANTS.TARGET) ? cell : CONSTANTS.NORMAL))
-      });
+    });
   }
-  redoWithoutAnimation(board){
+  componentDidMount(){
+    this.__resizeGrid();
+    window.addEventListener('resize', this.__resizeGrid);
+  }
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.__resizeGrid);
+  }
+  __animate = (animatorRef) => { /* Used Arrow function as this is going to be called in the animator class. Arrow functions bind 'this' to the context where the function is declared */
+    const nextFrame = this.frames.next();
+    let board;
+    if(nextFrame.done){
+      animatorRef.stop();
+      this.frames = null;
+      this.done = true;
+      if(nextFrame.value === null) alert("No pathh!!!");
+      return;
+    } 
+    
+    if(nextFrame.value.type === "visited"){
+      board = this.state.cells.map((row, i) => row.map((item, j) => nextFrame.value.value[[i, j]].visited && item === CONSTANTS.NORMAL ? CONSTANTS.VISITED : item))
+    }
+    else if(nextFrame.value.type === "path"){
+      board = this.__updateIndices([...nextFrame.value.value], [CONSTANTS.PATH], this.state.cells);
+    }
+    this.setState({cells:board});
+    this.forceUpdate();
+  }
+  __resizeGrid = () => {
+      this.clear();
+      const containerWidth = this.grid.current.offsetWidth;
+      const containerHeight = this.grid.current.offsetHeight;
+      const cols = Math.floor((containerWidth * 29)/1200);
+      const rows = Math.floor(containerHeight / (containerWidth / cols - 1));
+      this.setState({cells:this.__makeBoard(rows, cols)});
+      this.forceUpdate();
+  }
+  __canPlay(){
+    return this.isDragging === null;
+  }
+  __isInteractable(){
+    return this.frames === null;
+  }
+  __makeBoard(rows, columns){
+    const board = Array.from({length: rows},()=> Array.from({length: columns}, () => CONSTANTS.NORMAL));
+    this.source = [getRandomInt(0, rows), getRandomInt(0, columns / 2)]
+    do{
+      this.target = [getRandomInt(0, rows), getRandomInt(columns / 2 + 1, columns)]
+    }
+    while(this.target[0] === this.source[0] && this.target[1] === this.source[1]);
+    board[this.source[0]][this.source[1]] = CONSTANTS.SOURCE;
+    board[this.target[0]][this.target[1]] = CONSTANTS.TARGET;
+    return board;
+  }
+  __updateIndices(indices, values, array){
+    const board = [...array];
+    if(values.length === 1)
+      indices.map( index => board[index[0]][index[1]] = values[0]);
+    else if(values.length === indices.length)
+      indices.map( (index, i) => board[index[0]][index[1]] = values[i]);
+    else{
+      console.error("Error in updateIndices function. The length of valuelist is incompatible.");
+    }
+    return board;
+  }
+  __redoWithoutAnimation(board){
     const results = this.algorithms[this.algorithmChoice][1]();
     if(results !== null){
       board.forEach((row, i) => row.map((item, j) => (item === CONSTANTS.PATH || item === CONSTANTS.VISITED) && (board[i][j] = CONSTANTS.NORMAL))); /* Clear board of previously visited cells and paths */
@@ -135,13 +127,13 @@ export default class Board extends React.Component{
       results.path.forEach(index => board[index[0]][index[1]] = CONSTANTS.PATH);
       return board;
     } else{
-      // alert("No path!");
+        /* No path */
       return board;
     }
   }
 
-  normalAndWallDrag(row, column, val){
-    if(this.isInteractable() && this.isMouseDown){
+  __cellDragged(row, column, val){
+    if(this.__isInteractable() && this.isMouseDown){
       let board = null;
       
       if(this.isDragging !== null){
@@ -153,25 +145,25 @@ export default class Board extends React.Component{
       }
       
       else{
-        board = this.updateIndices([[row, column]], [val === CONSTANTS.WALL ? CONSTANTS.NORMAL : CONSTANTS.WALL], this.state.cells);
+        board = this.__updateIndices([[row, column]], [val === CONSTANTS.WALL ? CONSTANTS.NORMAL : CONSTANTS.WALL], this.state.cells);
       }
-      if(this.done) board = this.redoWithoutAnimation(board);
+      if(this.done) board = this.__redoWithoutAnimation(board);
       this.setState({ cells : board });
     }
   }
-  normalAndWallClick(row, column, val){
-    if(this.isInteractable()){
-      let board = this.updateIndices([[row, column]], [val === CONSTANTS.WALL ? CONSTANTS.NORMAL : CONSTANTS.WALL], this.state.cells);
-      if(this.done) board = this.redoWithoutAnimation(board);
+  __cellClicked(row, column, val){
+    if(this.__isInteractable()){
+      let board = this.__updateIndices([[row, column]], [val === CONSTANTS.WALL ? CONSTANTS.NORMAL : CONSTANTS.WALL], this.state.cells);
+      if(this.done) board = this.__redoWithoutAnimation(board);
       this.setState({ cells : board });
     }
   }
-  CellFactory(id, row, column){
+  __cellFactory(id, row, column){
     switch(id){
       case CONSTANTS.NORMAL : return <NormalCell 
                                         key = {[row, column]} 
-                                        onMouseMove = {() => this.normalAndWallDrag(row, column, CONSTANTS.NORMAL)}
-                                        onClick = {() => this.normalAndWallClick(row, column, CONSTANTS.NORMAL)}
+                                        onMouseMove = {() => this.__cellDragged(row, column, CONSTANTS.NORMAL)}
+                                        onClick = {() => this.__cellClicked(row, column, CONSTANTS.NORMAL)}
                                         />; /*Normal Cell*/
 
       case CONSTANTS.SOURCE : return <IconCell 
@@ -190,20 +182,20 @@ export default class Board extends React.Component{
 
       case CONSTANTS.WALL : return <Wall  
                                         key = {[row, column]} 
-                                        onMouseMove = {() => this.normalAndWallDrag(row, column, CONSTANTS.WALL)}
-                                        onClick = {() => this.normalAndWallClick(row, column, CONSTANTS.WALL)}
+                                        onMouseMove = {() => this.__cellDragged(row, column, CONSTANTS.WALL)}
+                                        onClick = {() => this.__cellClicked(row, column, CONSTANTS.WALL)}
                                       />; /* Wall */
       case CONSTANTS.PATH : return <Path 
                                         key = {[row, column]}
-                                        onMouseMove = {() => this.normalAndWallDrag(row, column, CONSTANTS.PATH)}
-                                        onClick = {() => this.normalAndWallClick(row, column, CONSTANTS.PATH)}                                        
+                                        onMouseMove = {() => this.__cellDragged(row, column, CONSTANTS.PATH)}
+                                        onClick = {() => this.__cellClicked(row, column, CONSTANTS.PATH)}                                        
                                         shouldAnimate = {!this.done}
                                       />; /* Path */
  
       case CONSTANTS.VISITED : return <VisitedCell 
                                         key = {[row, column]}
-                                        onMouseMove = {() => this.normalAndWallDrag(row, column, CONSTANTS.VISITED)}
-                                        onClick = {() => this.normalAndWallClick(row, column, CONSTANTS.VISITED)}
+                                        onMouseMove = {() => this.__cellDragged(row, column, CONSTANTS.VISITED)}
+                                        onClick = {() => this.__cellClicked(row, column, CONSTANTS.VISITED)}
                                         shouldAnimate = {!this.done}
                                       />; /* Visisted cell */
  
@@ -218,7 +210,7 @@ export default class Board extends React.Component{
               onMouseUp = {() => { this.isMouseDown = false; this.isDragging = null;}}
             >
           {
-            this.state.cells && this.state.cells.map((item, row) => item.map((subItem, column) => this.CellFactory(subItem, row, column)))
+            this.state.cells && this.state.cells.map((item, row) => item.map((subItem, column) => this.__cellFactory(subItem, row, column)))
           }
         </Grid>
     </React.Fragment>
